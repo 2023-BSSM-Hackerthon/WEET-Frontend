47a1b3a2f23eb9d5893255a1e34c3b80aeb19601
이희성 <ihuiseong@ihuiseong-ui-noteubug.local>
2023-12-27 19:51:54 +0900

    ADD :: Landing layout

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,10 +1,21 @@
+import Logo from "../../assets/svgs/Logo";
 import Header from "../../components/Header";
+import theme from "../../styles/theme";
 import * as S from "./style";
 
 const Main = () => {
   return (
     <S.Container>
       <Header />
+      <S.Contents>
+        <S.SubTitle>고민이 있으신가요?</S.SubTitle>
+        <S.Title>여러분의 고민을</S.Title>
+        <S.Wraapper>
+          <Logo width={100} color={theme.primary} />
+          <S.Title>에서 풀어보세요.</S.Title>
+        </S.Wraapper>
+        <S.Button>상담 신청하기</S.Button>
+      </S.Contents>
     </S.Container>
   );
 };
--- src/pages/Landing/style.ts
+++ src/pages/Landing/style.ts
@@ -1,6 +1,44 @@
 import styled from "styled-components";
+import { font } from "../../styles/font";
+import theme from "../../styles/theme";
 
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
+`;
+
+export const Contents = styled.div`
+  width: 100%;
+  padding-top: 140px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const SubTitle = styled.h1`
+  ${font.$H5};
+  color: ${theme.primary};
+  margin-bottom: 24px;
+`;
+
+export const Title = styled.h1`
+  ${font.$D3};
+`;
+
+export const Wraapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const Button = styled.button`
+  width: 180px;
+  height: 54px;
+  border: none;
+  border-radius: 15px;
+  margin-top: 30px;
+  color: ${theme.white};
+  ${font.$Button2};
+  background-color: ${theme.primary};
 `;
