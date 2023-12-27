46be356b5d5102aae4f284244f0f6c20fe0ec99d
이희성 <ihuiseong@ihuiseong-ui-noteubug.local>
2023-12-27 17:48:11 +0900

    ADD :: 절대경로

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { RecoilRoot } from "recoil";
 
 const App = () => {
   return (
-    <div>
+    <RecoilRoot>
       <h1>hello</h1>
-    </div>
+    </RecoilRoot>
   );
 };
 
