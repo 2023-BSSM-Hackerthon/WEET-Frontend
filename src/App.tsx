e8535b1205a9bb7f3897c86401fa43f88ec2028e
이희성 <ihuiseong@ihuiseong-ui-noteubug.local>
2023-12-27 19:02:32 +0900

    ADD :: useModal()

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import { RecoilRoot } from "recoil";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Modal from "./components/Modal";
 
 const App = () => {
   return (
-    <RecoilRoot>
-      <h1>hello</h1>
-    </RecoilRoot>
+    <>
+      <Modal />
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
