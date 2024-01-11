0cd1cd0c1aa762ba3f0c5743bf4cf0bab2199363
lheesung <lheesung.dev@gmail.com>
2024-01-11 17:38:07 +0900

    add :: 상담 신청 모달 퍼블리싱

--- src/components/AppointmentModal/index.tsx
+++ src/components/AppointmentModal/index.tsx
@@ -1,4 +1,6 @@
 import Logo from "../../assets/svgs/Logo";
+import Plus from "../../assets/svgs/Plus";
+import theme from "../../styles/theme/theme";
 import Horizon from "../../styles/ui/Horizon";
 import Text from "../../styles/ui/Text";
 import * as S from "./style";
@@ -15,6 +17,66 @@ const AppointmentModal = ({ closeModal }: ModalProps) => {
         <Text fontType="$Body2">상담 신청하기</Text>
       </S.Top>
       <Horizon />
+      <S.Contents>
+        {/*title*/}
+        <S.Wrapper>
+          <Text fontType="$Body3">상담 타이틀</Text>
+          <S.Input placeholder="상담 타이틀을 입력해주세요." maxLength={35} />
+        </S.Wrapper>
+
+        {/*contents*/}
+        <S.Wrapper>
+          <Text fontType="$Body3">상담 요청 내용</Text>
+          <S.TextArea placeholder="상담하시려는 내용을 적어주세요." />
+        </S.Wrapper>
+
+        {/*time*/}
+        <S.Wrapper>
+          <Text fontType="$Body3">상담시간</Text>
+          <S.TimeWrapper>
+            {/*year*/}
+            <S.TimeInput
+              placeholder="YYYY"
+              type="number"
+              min={2024}
+              max={2099}
+            />
+            <Text fontType="$H5" color={theme.grey[600]}>
+              .
+            </Text>
+
+            {/*month*/}
+            <S.TimeInput placeholder="MM" type="number" min={1} max={12} />
+            <Text fontType="$H5" color={theme.grey[600]}>
+              .
+            </Text>
+
+            {/*day*/}
+            <S.TimeInput placeholder="DD" type="number" min={1} max={31} />
+            <Text fontType="$H5" color={theme.grey[600]}>
+              .
+            </Text>
+
+            {/*time*/}
+            <S.TimeInput type="number" placeholder="HH:MM" />
+            <S.AddTime>
+              <Plus />
+            </S.AddTime>
+          </S.TimeWrapper>
+          <S.TimeWrapper>
+            <S.AddedTime>2024년11월17일14:30</S.AddedTime>
+            <S.AddedTime>2024년11월17일14:30</S.AddedTime>
+            <S.AddedTime>2024년11월17일14:30</S.AddedTime>
+          </S.TimeWrapper>
+        </S.Wrapper>
+      </S.Contents>
+      <S.Bottom>
+        <Horizon />
+        <S.BottomWrapper>
+          <S.Cancle>취소</S.Cancle>
+          <S.Button>신청하기</S.Button>
+        </S.BottomWrapper>
+      </S.Bottom>
     </S.Container>
   );
 };
--- src/components/AppointmentModal/style.ts
+++ src/components/AppointmentModal/style.ts
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 import theme from "../../styles/theme/theme";
+import { font } from "../../styles/theme/font";
 
 export const Container = styled.div`
   width: 600px;
-  height: 720px;
+  height: 740px;
   background-color: ${theme.white};
   border-radius: 20px;
-  padding: 30px 40px;
+  padding: 10px 20px;
   box-sizing: border-box;
 `;
 
@@ -17,4 +18,142 @@ export const Top = styled.div`
   justify-content: center;
   gap: 10px;
   margin-bottom: 18px;
+`;
+
+export const Contents = styled.div`
+  width: 100%;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 45px;
+  background-color: ${theme.grey[200]};
+  border-radius: 10px;
+  padding-left: 10px;
+  box-sizing: border-box;
+  ${font.$p1};
+  cursor: pointer;
+  &:hover {
+    background-color: ${theme.grey[300]};
+  }
+  transition: 0.3s ease-in-out;
+`;
+
+export const TextArea = styled.textarea`
+  width: 100%;
+  height: 200px;
+  border-radius: 10px;
+  resize: none;
+  padding: 10px;
+  box-sizing: border-box;
+  ${font.$p1};
+  background-color: ${theme.grey[200]};
+  &:hover {
+    background-color: ${theme.grey[300]};
+  }
+  transition: 0.3s ease-in-out;
+  cursor: pointer;
+`;
+
+export const TimeWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 8px;
+`;
+
+export const TimeInput = styled.input`
+  width: 100px;
+  height: 40px;
+  background-color: ${theme.grey[200]};
+  &:hover {
+    background-color: ${theme.grey[300]};
+  }
+  transition: 0.3s ease-in-out;
+  border-radius: 10px;
+  display: flex;
+  text-align: center;
+  ${font.$Body3};
+  &[type="number"]::-webkit-outer-spin-button,
+  &[type="number"]::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  box-sizing: border-box;
+`;
+
+export const AddTime = styled.button`
+  width: 45px;
+  height: 45px;
+  border-radius: 100px;
+  background-color: ${theme.primary};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &:hover {
+    filter: brightness(95%);
+  }
+  transition: 0.2s ease-in-out;
+`;
+
+export const AddedTime = styled.button`
+  width: 150px;
+  height: 40px;
+  background-color: ${theme.grey[200]};
+  border-radius: 5px;
+  ${font.$p2};
+  &:hover {
+    background-color: ${theme.warn};
+    color: ${theme.white};
+  }
+  transition: 0.2s ease-in-out;
+`;
+
+export const Bottom = styled.div`
+  width: 100%;
+  margin-top: 80px;
+`;
+
+export const Cancle = styled.button`
+  width: 70px;
+  height: 45px;
+  border-radius: 100px;
+  background-color: white;
+  border: 1px solid ${theme.grey[800]};
+  ${font.$Button3};
+  &:hover {
+    background-color: ${theme.grey[300]};
+    border: 1px solid ${theme.grey[500]};
+  }
+  transition: 0.2s ease-in-out;
+`;
+
+export const Button = styled.button`
+  width: 100px;
+  height: 45px;
+  border-radius: 100px;
+  background-color: ${theme.primary};
+  ${font.$Button3};
+  &:hover {
+    filter: brightness(95%);
+  }
+  transition: 0.2s ease-in-out;
+`;
+
+export const BottomWrapper = styled.div`
+  margin-top: 10px;
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  gap: 10px;
 `;
